1a781875e65e575a1c85a451ffbd4db9c867a1ec
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-11-17 16:44:55 +0500

    Initial commit

---
+++ src/test/java/sayyeed/dev/aiagenttelegrambot/AiAgentTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package sayyeed.dev.aiagenttelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
