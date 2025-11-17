9341e8e4f9431ea2327098ff29fd5b7789ef4346
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-11-17 17:04:29 +0500

    Project Setup

--- src/main/java/sayyeed/dev/aiagenttelegrambot/Application.java
+++ src/main/java/sayyeed/dev/aiagenttelegrambot/Application.java
@@ -1,10 +1,18 @@
 package sayyeed.dev.aiagenttelegrambot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .load();
+        dotenv.entries().forEach(e -> {
+            System.setProperty(e.getKey(), e.getValue());
+        });
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
